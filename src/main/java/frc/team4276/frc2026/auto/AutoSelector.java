31635db55c4451d006c3a42b8b7a3c8d14d0b9e2
Breadfest <ethanoskatt1@gmail.com>
2026-01-12 15:25:19 -0800

    add pathplanner; add auto utils

---
+++ src/main/java/frc/team4276/frc2026/auto/AutoSelector.java
@@ -0,0 +1,85 @@
+package frc.team4276.frc2026.auto;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.team4276.lib.AllianceFlipUtil;
+import frc.team4276.lib.VirtualSubsystem;
+import java.util.function.Supplier;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
+
+public class AutoSelector extends VirtualSubsystem {
+  private final AutoFactory autoFactory;
+
+  private final LoggedDashboardChooser<Supplier<Command>> routineChooser =
+      new LoggedDashboardChooser<>("Comp/Auto/RoutineChooser");
+  private Supplier<Command> lastRoutine = () -> Commands.none();
+  private String lastRoutineName = "";
+
+  private static boolean autoChanged = true;
+
+  private final LoggedNetworkNumber delayInput = new LoggedNetworkNumber("Comp/Auto/Delay", 0.0);
+
+  public AutoSelector(AutoFactory autoFactory) {
+    this.autoFactory = autoFactory;
+
+    routineChooser.addDefaultOption("Do Nothing", () -> this.autoFactory.idle());
+  }
+
+  /** Returns the selected auto command with the inputted delay. */
+  public Command getCommand() {
+    return lastRoutine
+        .get()
+        .beforeStarting(Commands.waitSeconds(getDelayInput()))
+        .finallyDo(() -> this.autoFactory.autoEnd());
+  }
+
+  public double getDelayInput() {
+    return delayInput.get();
+  }
+
+  private boolean wasRed = false;
+
+  public void periodic() {
+    // Skip updates when actively running in auto
+    if (DriverStation.isAutonomousEnabled() && lastRoutine != null) {
+      return;
+    }
+
+    SmartDashboard.putNumber("Comp/Auto/Delay Input Submitted ", getDelayInput());
+
+    // Update the list of questions
+    var routineName = routineChooser.getSendableChooser().getSelected();
+
+    // Update the routine and responses
+    if (lastRoutineName != routineName) {
+      var selectedRoutine = routineChooser.get();
+      if (selectedRoutine == null) {
+        return;
+      }
+
+      lastRoutine = selectedRoutine;
+      lastRoutineName = routineName;
+      autoChanged = true;
+    }
+
+    SmartDashboard.putString("Comp/Auto/Routine Submitted ", lastRoutineName);
+
+    if (AllianceFlipUtil.shouldFlip() != wasRed) {
+      autoChanged = true;
+    }
+
+    wasRed = AllianceFlipUtil.shouldFlip();
+  }
+
+  public static boolean hasAutoChanged() {
+    if (autoChanged) {
+      autoChanged = false;
+      return true;
+    }
+
+    return false;
+  }
+}
---
+++ src/main/java/frc/team4276/lib/path/PathUtil.java
@@ -0,0 +1,30 @@
+package frc.team4276.lib.path;
+
+import static frc.team4276.frc2026.constants.FieldConstants.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
+public class PathUtil {
+  public static Translation2d mirrorLengthwise(Translation2d trans) {
+    return new Translation2d(trans.getX(), fieldWidth - trans.getY());
+  }
+
+  public static Rotation2d mirrorLengthwise(Rotation2d trans) {
+    return trans.unaryMinus();
+  }
+
+  public static Pose2d mirrorLengthwise(Pose2d pose) {
+    return new Pose2d(
+        mirrorLengthwise(pose.getTranslation()), mirrorLengthwise(pose.getRotation()));
+  }
+
+  public static ChassisSpeeds mirrorLengthwise(ChassisSpeeds speeds) {
+    return new ChassisSpeeds(
+        speeds.vxMetersPerSecond,
+        -1.0 * speeds.vyMetersPerSecond,
+        -1.0 * speeds.omegaRadiansPerSecond);
+  }
+}
