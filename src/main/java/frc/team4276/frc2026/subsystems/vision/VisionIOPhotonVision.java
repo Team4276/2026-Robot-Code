8998dc553d85219239b18b87fb2af67999087d21
Breadfest <ethanoskatt1@gmail.com>
2026-01-13 08:45:06 -0800

    update photonlib

--- src/main/java/frc/team4276/frc2026/subsystems/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/team4276/frc2026/subsystems/vision/VisionIOPhotonVision.java
@@ -14,26 +14,30 @@
 package frc.team4276.frc2026.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
-// import frc.team4276.frc2026.field.FieldConstants;
 
 import static frc.team4276.frc2026.subsystems.vision.VisionConstants.*;
 
-// import java.util.HashSet;
-// import java.util.LinkedList;
-// import java.util.List;
-// import java.util.Optional;
-// import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 /** IO implementation for real PhotonVision hardware. */
 public class VisionIOPhotonVision implements VisionIO {
-  // protected final PhotonCamera camera;
+  protected final PhotonCamera camera;
   protected final Transform3d robotToCamera;
 
   private final Supplier<Pose2d> robotPoseSupplier;
-  // private final PhotonPoseEstimator poseEstimator;
+  private final PhotonPoseEstimator poseEstimator;
 
   /**
    * Creates a new VisionIOPhotonVision.
@@ -42,78 +46,77 @@ public class VisionIOPhotonVision implements VisionIO {
    * @param rotationSupplier The 3D position of the camera relative to the robot.
    */
   public VisionIOPhotonVision(int index, Supplier<Pose2d> robotPoseSupplier) {
-    // camera = new PhotonCamera(configs[index].name);
+    camera = new PhotonCamera(configs[index].name);
     this.robotToCamera = configs[index].robotToCamera;
     this.robotPoseSupplier = robotPoseSupplier;
 
-    // poseEstimator = new PhotonPoseEstimator(aprilTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCamera);
-    // poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+    poseEstimator = new PhotonPoseEstimator(aprilTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCamera);
+    poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
   }
 
   @Override
   public void updateInputs(VisionIOInputs inputs) {
-    // inputs.connected = camera.isConnected();
+    inputs.connected = camera.isConnected();
 
-    // // For single tag ambiguity correct, feed in full estimated pose
-    // poseEstimator.setReferencePose(robotPoseSupplier.get());
+    // For single tag ambiguity correct, feed in full estimated pose
+    poseEstimator.setReferencePose(robotPoseSupplier.get());
 
-    // // Read new camera observations
-    // Set<Short> tagIds = new HashSet<>();
-    // List<PoseObservation> poseObservations = new LinkedList<>();
-    // for (var result : camera.getAllUnreadResults()) {
-    //   // Update latest target observation
-    //   if (result.hasTargets()) {
-    //     inputs.latestTargetObservation = new TargetObservation(
-    //         Rotation2d.fromDegrees(result.getBestTarget().getYaw()),
-    //         Rotation2d.fromDegrees(result.getBestTarget().getPitch()));
-    //   } else {
-    //     inputs.latestTargetObservation = new TargetObservation(new Rotation2d(), new Rotation2d());
-    //   }
+    // Read new camera observations
+    Set<Short> tagIds = new HashSet<>();
+    List<PoseObservation> poseObservations = new LinkedList<>();
+    for (var result : camera.getAllUnreadResults()) {
+      // Update latest target observation
+      if (result.hasTargets()) {
+        inputs.latestTargetObservation = new TargetObservation(
+            Rotation2d.fromDegrees(result.getBestTarget().getYaw()),
+            Rotation2d.fromDegrees(result.getBestTarget().getPitch()));
+      } else {
+        inputs.latestTargetObservation = new TargetObservation(new Rotation2d(), new Rotation2d());
+      }
 
-    //   Optional<EstimatedRobotPose> estimate = Optional.empty()
-    //   // poseEstimator.update(result);
+      Optional<EstimatedRobotPose> estimate = poseEstimator.update(result);
 
-    //   estimate.ifPresent(
-    //       (poseEstimate) -> {
-    //         if (rejectEstimate(poseEstimate)) {
-    //           return;
-    //         }
+      estimate.ifPresent(
+          (poseEstimate) -> {
+            if (rejectEstimate(poseEstimate)) {
+              return;
+            }
 
-    //         // Calculate average tag distance
-    //         double totalTagDistance = 0.0;
-    //         for (var target : poseEstimate.targetsUsed) {
-    //           // Add tag IDs
-    //           tagIds.add((short) target.fiducialId);
-    //           totalTagDistance += target.bestCameraToTarget.getTranslation().getNorm();
-    //         }
+            // Calculate average tag distance
+            double totalTagDistance = 0.0;
+            for (var target : poseEstimate.targetsUsed) {
+              // Add tag IDs
+              tagIds.add((short) target.fiducialId);
+              totalTagDistance += target.bestCameraToTarget.getTranslation().getNorm();
+            }
 
-    //         // Add observation
-    //         poseObservations.add(
-    //             new PoseObservation(
-    //                 poseEstimate.timestampSeconds, // Timestamp
-    //                 poseEstimate.estimatedPose, // 3D pose estimate
-    //                 poseEstimate.targetsUsed.get(0).poseAmbiguity, // Ambiguity
-    //                 poseEstimate.targetsUsed.size(), // Tag count
-    //                 totalTagDistance / poseEstimate.targetsUsed.size(), // Average tag distance
-    //                 PoseObservationType.PHOTONVISION)); // Observation type
-    //       });
-    // }
+            // Add observation
+            poseObservations.add(
+                new PoseObservation(
+                    poseEstimate.timestampSeconds, // Timestamp
+                    poseEstimate.estimatedPose, // 3D pose estimate
+                    poseEstimate.targetsUsed.get(0).poseAmbiguity, // Ambiguity
+                    poseEstimate.targetsUsed.size(), // Tag count
+                    totalTagDistance / poseEstimate.targetsUsed.size(), // Average tag distance
+                    PoseObservationType.PHOTONVISION)); // Observation type
+          });
+    }
 
-    // // Save pose observations to inputs object
-    // inputs.poseObservations = new PoseObservation[poseObservations.size()];
-    // for (int i = 0; i < poseObservations.size(); i++) {
-    //   inputs.poseObservations[i] = poseObservations.get(i);
-    // }
+    // Save pose observations to inputs object
+    inputs.poseObservations = new PoseObservation[poseObservations.size()];
+    for (int i = 0; i < poseObservations.size(); i++) {
+      inputs.poseObservations[i] = poseObservations.get(i);
+    }
 
-    // // Save tag IDs to inputs objects
-    // inputs.tagIds = new int[tagIds.size()];
-    // int i = 0;
-    // for (int id : tagIds) {
-    //   inputs.tagIds[i++] = id;
-    // }
+    // Save tag IDs to inputs objects
+    inputs.tagIds = new int[tagIds.size()];
+    int i = 0;
+    for (int id : tagIds) {
+      inputs.tagIds[i++] = id;
+    }
   }
 
-  // private boolean rejectEstimate(EstimatedRobotPose estimate) {
-    
-  // }
+  private boolean rejectEstimate(EstimatedRobotPose estimate) {
+    return false;
+  }
 }
