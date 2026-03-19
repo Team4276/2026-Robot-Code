7786f7685f7ab5aeb045f1a69462de9adeee1d9c
Breadfest <ethanoskatt1@gmail.com>
2026-03-18 19:25:16 -0700

    whoops

--- src/main/java/frc/team4276/frc2026/subsystems/flywheel/FlywheelIOTalon.java
+++ src/main/java/frc/team4276/frc2026/subsystems/flywheel/FlywheelIOTalon.java
@@ -42,7 +42,7 @@ public class FlywheelIOTalon implements FlywheelIO {
     private boolean brakeModeEnabled = false;
 
     public FlywheelIOTalon() {
-        talon = TalonFXFactory.createDefaultTalon(Ports.FLYWHEEL_RIGHT, CanBus.CANIVORE);
+        talon = TalonFXFactory.createDefaultTalon(Ports.FLYWHEEL_RIGHT, CanBus.RIO);
         followerTalon = TalonFXFactory.createPermanentFollowerTalon(Ports.FLYWHEEL_LEFT, Ports.FLYWHEEL_RIGHT,
                 CanBus.CANIVORE, true);
 
